29d1dd9ec70a95150a911bc945ffc1eec252bd98
Adrian <148253121+AdrianC0712@users.noreply.github.com>
2024-10-01 20:05:41 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double price = 3.56;
+        double tax = 5.45;
+        int quantity = 5;
+        double total = 0;
+        String message = "I want to buy " + quantity + " shirt!";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(message);
+        total= price * quantity * tax;
+        System.out.println("Total cost with tax is: " + total);
+
     }
 }
